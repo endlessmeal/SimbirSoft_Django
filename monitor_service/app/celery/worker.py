04c4e68efa3dc0345e9dd8d2c8979d8dda384c71
endlessmeal <baloyan99@list.ru>
2020-08-13 16:33:26 +0300

    reformat some code

--- monitor_service/app/celery/worker.py
+++ monitor_service/app/celery/worker.py
@@ -1,7 +1,5 @@
 from celery import Celery
 from ..config import BROKER_CONN_URI
 
-celery = Celery(
-    "statistics", broker=BROKER_CONN_URI, include=["app.celery.tasks"]
-)
+celery = Celery("statistics", broker=BROKER_CONN_URI, include=["app.celery.tasks"])
 celery.autodiscover_tasks(["app", "celery"])

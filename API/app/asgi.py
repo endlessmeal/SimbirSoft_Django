94708f53a1d2dcdfde8e6c29d75ac70365c2c1c1
endlessmeal <baloyan99@list.ru>
2020-08-28 20:38:08 +0300

    Opps

--- API/app/asgi.py
+++ API/app/asgi.py
@@ -15,13 +15,13 @@ async def send_stats(request: Request, call_next):
     process_time = float(datetime.now().timestamp()) - start_time
     url = request.url.path
     status_code = response.status_code
-    req = {
-        "service": service,
-        "url": url,
-        "status_code": status_code,
-        "response_time": process_time,
-        "request_timestamp": start_time,
-    }
+    req = dict(
+        service=service,
+        url=url,
+        status_code=status_code,
+        response_time=process_time,
+        request_timestamp=start_time,
+    )
     async with httpx.AsyncClient() as client:
         await client.post(f"{monitor_url}/api/v1/send/", json=req)
 
